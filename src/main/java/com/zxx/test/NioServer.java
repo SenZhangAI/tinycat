f6447029d46b55ae741a662abf5999c3f3eeb170
zengxiuxiong <2601051929@qq.com>
2024-04-17 15:16:37 +0800

    first commit

---
+++ src/main/java/com/zxx/test/NioServer.java
@@ -0,0 +1,75 @@
+package com.zxx.test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NioServer {
+
+    static void startServer(int port) throws IOException {
+        //创建NIO ServerSocketChannel
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(port));
+        //设置ServerSocketChannel为非阻塞
+        serverSocketChannel.configureBlocking(false);
+        //打开Selector处理Channel,即创建epoll
+        Selector selector = Selector.open();
+        //把ServerSocketChannel注册到selector上，并且selector对客户端accept连接操作感兴趣
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        System.out.println("服务器启动成功");
+
+        while (true) {
+            //阻塞等待需要处理的事情发生 (注意是阻塞等待，如果没有注册的事件发生会处于休眠状态，不会占用CPU资源)
+            selector.select();
+
+            //获取selector中注册的全部时间的SelectionKey实例
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+            //遍历SelectionKey对事件进行处理
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                //如果是OP_ACCEPT事件，则进行连接获取和事件注册
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = server.accept();
+                    socketChannel.configureBlocking(false);
+                    // 这里只注册了读事件，如果需要给客户端发送数据可以注册写事件
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    System.out.println("客户端连接成功");
+                } else if (key.isReadable()) {
+                    //如果是OP_READ事件，则进行读取和打印
+                    SocketChannel server = (SocketChannel) key.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    int len = server.read(byteBuffer);
+                    //如果有数据，把数据打印出来
+                    if (len > 0) {
+                        byteBuffer.flip();
+                        byte[] bytes = new byte[byteBuffer.remaining()];
+                        byteBuffer.get(bytes);
+                        String message = new String(bytes);
+                        System.out.println("接收到消息：" + message);
+
+                        String responseData = "Hello World\n";
+                        ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                        writeBuffer.put(responseData.getBytes());
+                        writeBuffer.flip();
+                        server.write(writeBuffer);
+                    } else if (len == -1) {
+                        //如果客户端断开连接，关闭socket
+                        System.out.println("客户端断开连接");
+                        server.close();
+                    }
+                }
+                //从事件集合里删除本次处理的key,防止下次select重复使用
+                iterator.remove();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/zxx/test/Test.java
@@ -0,0 +1,9 @@
+package com.zxx.test;
+
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        NioServer.startServer(8080);
+    }
+}
